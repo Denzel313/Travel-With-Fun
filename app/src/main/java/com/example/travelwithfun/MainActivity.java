a037c96cce32324d03b80cec64c3b62814f53f01
Denzel313 <maple4064@gmail.com>
2023-10-16 09:37:02 +0700

    Change main activity to splash screen

--- app/src/main/java/com/example/travelwithfun/MainActivity.java
+++ app/src/main/java/com/example/travelwithfun/MainActivity.java
@@ -5,27 +5,47 @@ import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isAndroidReady = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //handle splash screen transition
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
+        //keep splashscreen for a long time
+        View content = findViewById(android.R.id.content);
+        //add the pre draw listener to the tree view observer
+        content.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                if (isAndroidReady) {
+                    content.getViewTreeObserver().removeOnPreDrawListener(this);
+                }
+
+                //define a method to alter isAndroidReady boolean
+                dismissSplashScreen();
+                return false;
+            }
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Splash Screen
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+    }
+
+    private void dismissSplashScreen() {
+        //use handler
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                splashScreen.setKeepOnScreenCondition(new SplashScreen.KeepOnScreenCondition() {
-                    @Override
-                    public boolean shouldKeepOnScreen() {
-                        return false;
-                    }
-                });
+                //change the boolean
+                isAndroidReady = true;
             }
         }, 2000);
-
     }
 }
